da83d053725e72ce46415fbb32ba6a9dabb87550
not-zeby <166417078+not-zeby@users.noreply.github.com>
2024-04-08 17:24:05 +0200

    Update videotaxi.php

--- videotaxi.php
+++ videotaxi.php
@@ -8,25 +8,24 @@ Author: Sebastian Streich
 Author URI: https://github.com/not-zeby
 */
 
-// Shortcode für VideoTaxi-Videoeinbettung
+// create shortcode element
 function videotaxi_embed_video($atts) {
-    // Standardwerte für den Shortcode
     $atts = shortcode_atts(array(
         'id' => '',
     ), $atts);
 
-    // Überprüfen, ob eine Video-ID im Shortcode angegeben wurde
+    // Check for VideoTaxi ID
     if (empty($atts['id'])) {
-        return '<p style="color: red;">Fehler: Bitte geben Sie eine Video-ID im Shortcode an!</p>';
+        return '<p style="color: red;">Error: Please provide a VideoTaxi ID!</p>';
     }
 
-    // Video-URL erstellen
+    // Create URL
     $video_url = 'https://media.video.taxi/embed/' . sanitize_text_field($atts['id']);
 
-    // CSS für responsives iFrame
+    // CSS for responsive iFrame
     $css = '<style>.videotaxi-embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .videotaxi-embed-container iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>';
 
-    // iFrame-Code generieren
+    // Generate iFrame
     $iframe_code = '<div class="videotaxi-embed-container"><iframe src="' . esc_url($video_url) . '" frameborder="0" allowfullscreen></iframe></div>';
 
     return $css . $iframe_code;
